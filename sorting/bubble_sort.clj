c696231d150a3611bfd0b9e2a470245d73fdcfb7
Michelly Menezes <martha.michelly@gmail.com>
2018-10-28 22:59:11 -0300

    add bubble sort script

---
+++ sorting/bubble_sort.clj
@@ -0,0 +1,14 @@
+(ns sorting/bubble_sort)
+
+(defn- bubble [ys x]
+  (if-let [y (peek ys)]
+    (if (> y x)
+      (conj (pop ys) x y)
+      (conj ys x))
+    [x]))
+
+(defn bubble-sort [xs]
+  (let [ys (reduce bubble [] xs)]
+    (if (= xs ys)
+      xs
+      (recur ys))))
