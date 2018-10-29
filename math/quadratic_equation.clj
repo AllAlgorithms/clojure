d4af60923d71866a68952256fcdbd0c31c3ede43
Michelly Menezes <martha.michelly@gmail.com>
2018-10-28 21:49:02 -0300

    add quadratic equation function

---
+++ math/quadratic_equation.clj
@@ -0,0 +1,14 @@
+(ns math/quadratic_equation)
+
+(defn calculate-delta
+      [a b c]
+      (- (* b b) (* 4 a c)))
+
+(defn calculate-x [delta a b]
+      (conj #{} (-> b - (+ (Math/sqrt delta)) (/ (* 2 a)))
+                (-> b - (- (Math/sqrt delta)) (/ (* 2 a)))))
+
+(defn quadratic-equation [a b c]
+      (let [delta (calculate-delta a b c)]
+           (when (>= delta 0)
+             (calculate-x delta a b))))
